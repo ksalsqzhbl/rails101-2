7fb090a569160d3c8a1cd2611c467b67dcc8b005
你的名字 <ksalsqzhbl@aliyun.com>
2016-12-30 10:42:50 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,7 +18,9 @@ before_action :authenticate_user! , only: [:new]
 end
 
  def create
-    @group = Group.new(group_params)
+   @group = Group.new(group_params)
+    @group.user = current_user
+
     if @group.save
       redirect_to groups_path
     else
